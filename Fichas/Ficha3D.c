ec04f75e6388b77065cd24991bfe6d1245369ae8
mrmikept <mike.pinto1507@gmail.com>
2022-03-10 23:28:22 +0000

    Update

--- Fichas/Ficha3D.c
+++ Fichas/Ficha3D.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-
-int verPrimo();
-int verTriang();
-int main() {
-    int v1, v2, i; //v1 e v2 -> valor min e max para procurar primos e numeros triangulares // i -> valor usado para o ciclo for.
-    if (scanf("%d %d",&v1,&v2) == 2) {
-       if (v1 > v2) { //Troca de v1 por v2 se v1 > v2 (Ordenação.)
-        int c = v1;
-        v1 = v2;
-        v2 = c;
-       }
-       int np=0, nt=0, primo=0, triang=0;
-       primo = verPrimo(v1, v2);
-       triang = verTriang(v1, v2);
-    printf("%d %d\n",triang, primo);
-    }
-    return 0;
-}
-
-int verPrimo(int n) { //NUmeros primos são aqueles que somente são divisiveis por 1 e por ele mesmo. Então vamos usar um ciclo de for para verificar se existe
-    int x, primo = 1;    //mais algum numero que possa dividir aquele numero, se houve então esta função retorna 0(False) se não houve então retorna 1(True)
-    for (x = 2; x <= n/2; ++x) {
-        if (n % x == 0) {
-            primo = 0;
-            break;
-        }
-    }
-    return primo;
-}
-
-int verTriang(int n,int b) {
-    for (int j = 0; j <= n; j++)
+int main()
+{
+    int i, j, v1, v2, primo = 0, triangular = 0;
+    if ((scanf("%d %d", &v1, &v2)) == 2)
     {
-        if ((((j+1)*j)/2) == n)
+        bool *primes = malloc(sizeof(bool) * (v2 + 1)); // Alocar um array com o tamanho maximo fornecido (v2).
+
+        for (i = 2; i <= v2; i++) // Inicializar o array a true
+            primes[i] = true;
+
+        i = 2;
+        while ((i * i) <= v2) // Criação do crivo de eratostenes
         {
-            return 1;
-            break;
+            if (primes[i] != false)
+            {
+                for (j = 2; j < v2; j++)
+                {
+                    if ((primes[i] ? i : 0) * j > v2)
+                        break;
+                    else
+                        primes[(primes[i] ? i : 0) * j] = false;
+                }
+            }
+            i++;
         }
-        
+
+        while (v1 <= v2)
+        {
+            // Conta todos os numeros primos usando o crivo. Ou seja, incrementa se a posição no array estiver a false.
+            if (primes[v1] != false)
+            {
+                primo++;
+            }
+            // Conta todos os numeros triangulares.
+            int a = sqrt(2 * v1 + 0.25) - 0.5;
+            int b = a;
+            if (b == a && ((b * (b + 1)) / 2) == v1)
+                triangular++;
+
+            v1++;
+        }
+        free(primes);
     }
+    printf("%d %d\n", triangular, primo);
     return 0;
 }
